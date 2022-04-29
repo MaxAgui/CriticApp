e3923ade0fd7b07a8402f412a669b6b3db5115b5
MaxAgui <manuelmaximo19@gmail.com>
2022-04-29 15:17:35 -0500

    ADD: user critics counter cache

--- app/models/critic.rb
+++ app/models/critic.rb
@@ -1,4 +1,4 @@
 class Critic < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, counter_cache: true
   belongs_to :criticable, polymorphic: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_29_195017) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_29_201536) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -85,7 +85,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_29_195017) do
     t.string "username"
     t.string "email"
     t.string "role"
-    t.integer "critics_count"
+    t.integer "critics_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
