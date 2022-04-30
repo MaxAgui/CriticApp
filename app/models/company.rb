32fc47f11991f12aabd4e0b3517fb23215677be7
Julio Bazalar <jbzuloaga@gmail.com>
2022-04-30 16:58:33 -0500

    ADD: logic for game's cover

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,7 +1,8 @@
 class Company < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :critics, as: :criticable, dependent: :destroy
-  
+
   has_many :involved_companies, dependent: :destroy
   has_many :games, through: :involved_companies
+  has_one_attached :cover
 end
