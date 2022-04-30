39d0dcc2a366310c11de82fe5b61bbbbf6d6bd75
Denis Nahuamel <denis123nahuamel@github.com>
2022-04-30 15:45:29 -0500

    add views to devise

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,10 @@ Rails.application.routes.draw do
   devise_for :users 
   post "/new_user", to: "users#create"
   get "/new_user", to: "users#new"
-  # get "/new_user", to: "users#create"
-  # resources :users, only: %i[new, show]
-   resources :users
-  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users
+
 
   # Defines the root path route ("/")
-  post "auth/github", as: :github_login
+  # post "auth/github", as: :github_login
+  # get "auth/github/callback", to: "sessions:create"
 end
